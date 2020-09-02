07ef9afa32d25a823195b7c01e0289ac4f000c25
Anthoy <AnthonyGrant200@hotmail.com>
2020-09-01 19:48:56 -0500

    testing pull request

--- Test_Cases.py
+++ Test_Cases.py
@@ -3,7 +3,9 @@ import time
 from Yahoo_Page import *
 from Tools import *
 
-class HomePageSetup(unittest.TestCase):        
+class HomePageSetup(unittest.TestCase):
+
+    #THIS HAS CHANGED        
 
     #I initially had random password method here, moved it closer to where it is used
         

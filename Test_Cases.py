62ad891943a2349b26616f784ee79a406d967546
Tony Grant <AnthonyGrant200@hotmail.com>
2020-09-14 22:29:51 -0500

    Update Test_Cases.py

--- Test_Cases.py
+++ Test_Cases.py
@@ -5,8 +5,6 @@ from Tools import *
 
 class HomePageSetup(unittest.TestCase):
 
-    #THIS HAS CHANGED  Please show this times 3     
-
     #I initially had random password method here, moved it closer to where it is used
         
     def setUp(self):

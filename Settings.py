6d3c4141799b4f1b153f0307299308d9cf12c790
Anthoy <AnthonyGrant200@hotmail.com>
2020-10-19 01:26:41 -0500

    fixed generate random string tool 10.19.20

--- Settings.py
+++ Settings.py
@@ -1,13 +1,13 @@
-path_to_adBlock = r""
-path_to_webdriver = r""
+path_to_adBlock = r"C:\Repositories\Website_Automation_python\Resources\adblocker\3.9_0"
+path_to_webdriver = r"C:\Repositories\Website_Automation_python\Resources\windows chromedriver\chromedriver.exe"
 
-yahoo_username = ""
-yahoo_password = ""
+yahoo_username = "anthonygrant414@yahoo.com"
+yahoo_password = "yellow613@"
 
 
 
 #this is just for me, my Secure.py file is in .gitignore
-try:
+''' try:
     from Secure import *
 except Exception:
-    pass
+    pass '''

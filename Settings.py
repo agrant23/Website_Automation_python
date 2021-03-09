023569419f060cd9ffed11fa6821e33a2d069397
Tony Grant <AnthonyGrant200@hotmail.com>
2021-03-09 17:35:03 -0600

    Update Settings.py

--- Settings.py
+++ Settings.py
@@ -1,15 +1,6 @@
 path_to_adBlock = r"your\path\here\Resources\adblocker\3.9_0"
 path_to_webdriver = r"your\path\here\Resources\windows chromedriver\chromedriver.exe"
 
-<<<<<<< HEAD
-yahoo_username = "anthonygrant414@yahoo.com"
-yahoo_password = "yellow613@G!24"
-=======
-yahoo_username = ""
-yahoo_password = ""
->>>>>>> 808fc79399c662936a4ddd8f81222d64fd535e17
-
-
 
 #this is just for me, my Secure.py file is in .gitignore
 try:

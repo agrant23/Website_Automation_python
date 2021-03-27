708e290e368b96b13b48ad27fa3c3ee8236ae6ca
Anthoy <AnthonyGrant200@hotmail.com>
2021-03-26 21:45:17 -0500

    update

--- Settings.py
+++ Settings.py
@@ -1,10 +1,10 @@
-import Secure
+import secure
 # replace your path with the Secure path below
 
-path_to_adBlock = Secure.path_to_adBlock + r"\Resources\adblocker\3.9_0"
+path_to_adBlock = secure.path_to_adBlock + r"\Resources\adblocker\3.9_0"
 path_to_webdriver = (
- Secure.path_to_webDriver + r"\Resources\windows chromedriver\chromedriver.exe"
+ secure.path_to_webDriver + r"\Resources\windows chromedriver\chromedriver.exe"
 )
 
-yahoo_username = Secure.yahoo_username
-yahoo_password = Secure.yahoo_password
+yahoo_username = secure.yahoo_username
+yahoo_password = secure.yahoo_password
